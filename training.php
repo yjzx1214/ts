2c1baf9e87d2b9a0719d8a0b2009397dbb4bcb4c
Dylan <9025681+FluffyMan47@users.noreply.github.com>
2022-09-20 14:35:35 +1000

    Changed Training.php

--- training.php
+++ training.php
@@ -25,10 +25,58 @@ if (isset($_SESSION['islogin'])) {
 
     <!-- Title-->
 <main>
-      <div style="text-align:center">
+  <div style="text-align:center">
             <h1>Training</h1>
         </div>
-        <div class="TrainingContainer">
+
+
+        <div id="myBtnContainer">
+            <button class="Trainingbtn active" onclick="filterSelection('all')"> Show all</button>
+            <button class="Trainingbtn" onclick="filterSelection('SS')"> Strategic Studies</button>
+            <button class="Trainingbtn" onclick="filterSelection('IT')"> IT</button>
+            <button class="Trainingbtn" onclick="filterSelection('Cyber')"> Cyber Security</button>
+            <button class="Trainingbtn" onclick="filterSelection('Finance')"> Finance</button>
+            <button class="Trainingbtn" onclick="filterSelection('Governance')"> Governance</button>
+        </div>
+
+        <div class="TrainingContainer SS">
+
+            <div class="TrainingSubTitle">
+                <h3>Strategic Studies</h3>
+            </div>
+
+            <div class="TrainingContent">
+                <div class="TrainingImg">
+                    <h1>Stuff</h1>
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                </p>
+                <input type="submit" value="Submit" class="JoinClass">
+                <input type="submit" value="Change" class="JoinClass">
+            </div>
+
+            <div class="TrainingContent">
+                <div class="TrainingImg">
+                    <h1>Stuff</h1>
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                </p>
+                <input type="submit" value="Submit" class="JoinClass">
+            </div>
+        </div>
+
+        <div class="TrainingContainer IT">
+
+            <div class="TrainingSubTitle">
+                <h3>IT</h3>
+            </div>
+
             <div class="TrainingContent">
                 <div class="TrainingImg">
                     <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
@@ -41,11 +89,119 @@ if (isset($_SESSION['islogin'])) {
                 <input type="submit" value="Submit" class="JoinClass">
                 <input type="submit" value="Change" class="JoinClass">
             </div>
-
         </div>
 
+        <div class="TrainingContainer Cyber">
+
+            <div class="TrainingSubTitle">
+                <h3>Cyber Security</h3>
+            </div>
+
+            <div class="TrainingContent">
+                <div class="TrainingImg">
+                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                </p>
+                <input type="submit" value="Submit" class="JoinClass">
+                <input type="submit" value="Change" class="JoinClass">
+            </div>
+        </div>
+
+        <div class="TrainingContainer Finance">
+
+            <div class="TrainingSubTitle">
+                <h3>Finance</h3>
+            </div>
+            <div class="TrainingContent">
+                <div class="TrainingImg">
+                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                </p>
+                <input type="submit" value="Submit" class="JoinClass">
+                <input type="submit" value="Change" class="JoinClass">
+            </div>
+        </div>
+
+        <div class="TrainingContainer Governance">
+
+            <div class="TrainingSubTitle">
+                <h3>Governance</h3>
+            </div>
+
+            <div class="TrainingContent">
+                <div class="TrainingImg">
+                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
+                </p>
+                <input type="submit" value="Submit" class="JoinClass">
+                <input type="submit" value="Change" class="JoinClass">
+            </div>
+        </div>
+
+
+
+        <script>
+            filterSelection("all")
+            function filterSelection(c) {
+                var x, i;
+                x = document.getElementsByClassName("TrainingContainer");
+                if (c == "all") c = "";
+                for (i = 0; i < x.length; i++) {
+                    w3RemoveClass(x[i], "show");
+                    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+                }
+            }
+
+            function w3AddClass(element, name) {
+                var i, arr1, arr2;
+                arr1 = element.className.split(" ");
+                arr2 = name.split(" ");
+                for (i = 0; i < arr2.length; i++) {
+                    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+                }
+            }
+
+            function w3RemoveClass(element, name) {
+                var i, arr1, arr2;
+                arr1 = element.className.split(" ");
+                arr2 = name.split(" ");
+                for (i = 0; i < arr2.length; i++) {
+                    while (arr1.indexOf(arr2[i]) > -1) {
+                        arr1.splice(arr1.indexOf(arr2[i]), 1);
+                    }
+                }
+                element.className = arr1.join(" ");
+            }
+
+
+            // Add active class to the current button (highlight it)
+            var btnContainer = document.getElementById("myBtnContainer");
+            var btns = btnContainer.getElementsByClassName("Trainingbtn");
+            for (var i = 0; i < btns.length; i++) {
+                btns[i].addEventListener("click", function () {
+                    var current = document.getElementsByClassName("active");
+                    current[0].className = current[0].className.replace(" active", "");
+                    this.className += " active";
+                });
+            }
+        </script>
 
 </main>
 
     <!-- This is footer file -->
     <?php include("footer.php"); ?>
+
+
+
