6e504ceb706452d1f4755ec24d857400ec7ab1d6
Dylan <9025681+FluffyMan47@users.noreply.github.com>
2022-10-18 14:25:27 +1100

    Training Changed to work with php

--- training.php
+++ training.php
@@ -23,13 +23,12 @@ if (isset($_SESSION['islogin'])) {
     <!-- This is nav bar file -->
     <?php include('nav.php') ?>
 
-    <!-- Title-->
-<main>
-  <div style="text-align:center">
+    <main>
+
+
+        <div style="text-align:center">
             <h1>Training</h1>
         </div>
-
-
         <div id="myBtnContainer">
             <button class="Trainingbtn active" onclick="filterSelection('all')"> Show all</button>
             <button class="Trainingbtn" onclick="filterSelection('SS')"> Strategic Studies</button>
@@ -39,185 +38,135 @@ if (isset($_SESSION['islogin'])) {
             <button class="Trainingbtn" onclick="filterSelection('Governance')"> Governance</button>
         </div>
 
-        <div id="addbtnContainer"
+        <div id="addbtnContainer">
             <div class=" addTrainingbtn">
-				<button class="Trainingbtn" onclick="document.getElementById('id10').style.display='block'" style="width:auto;">Add Class</button>
+				<button class="Trainingbtn" onclick="document.getElementById('id47').style.display='block'" style="width:auto;">Add Class</button>
             </div>
         </div>
 
-        <div id="id47" class="modal">
-            <form class="modal-content"  method="POST">
-                <div class="imgcontainer">
-                    <span onclick="document.getElementById('id47').style.display='none'" class="close" title="Close Modal">&times;</span>
-                </div>
-                <div class="container">
-                    <h1>Add Class</h1>
-                    <label for="category">Category</label>
-                    <input type="text" id="category" name="category" placeholder="Enter Category of class">
-                    <label for="course">Course</label>
-                    <input type="text" id="course" name="course" placeholder="Enter Course name">
-                    <label for="courseNum">Course Number</label>
-                    <input type="text" id="courseNum" name="courseNum" placeholder="Enter Course Number">
-                    <label for="cost">Cost</label>
-                    <input type="text" id="cost" name="cost" placeholder="Enter Cost of class">
-                    <label for="trainer">Trainer</label>
-                    <input type="text" id="trainer" name="trainer" placeholder="Enter Trainer of class">
-                    <label for="trainerEmail">Trainer Email</label>
-                    <input type="text" id="trainerEmail" name="trainerEmail" placeholder="Enter Email of Trainer">
-                    <label for="info">Information</label>
-                    <textarea id="info" name="info" placeholder="Enter Information about class" style="height:170px"></textarea>
-                    <input type="submit" value="Add Class">
-                    <button type="button" class="cancelbtn" onclick="document.getElementById('id47').style.display='none'" class="cancelbtn">Cancel</button>
-                </div>
-            </form>
-        </div>
+<div id="id47" class="modal">
+	<form class="modal-content"  method="POST">
+	        <div class="imgcontainer">
+				<span onclick="document.getElementById('id47').style.display='none'" class="close" title="Close Modal">&times;</span>
+			</div>
+		<div class="container">
+			<h1>Add Class</h1>
+			<label for="category">Category</label>
+			<input type="text" id="category" name="category" placeholder="Enter Category of class">
+            <label for="course">Course</label>
+			<input type="text" id="course" name="course" placeholder="Enter Course name">
+            <label for="courseNum">Course Number</label>
+			<input type="text" id="courseNum" name="courseNum" placeholder="Enter Course Number">
+			<label for="cost">Cost</label>
+			<input type="text" id="cost" name="cost" placeholder="Enter Cost of class">
+            <label for="trainer">Trainer</label>
+			<input type="text" id="trainer" name="trainer" placeholder="Enter Trainer of class">
+            <label for="trainerEmail">Trainer Email</label>
+			<input type="text" id="trainerEmail" name="trainerEmail" placeholder="Enter Email of Trainer">
+			<label for="info">Information</label>
+			<textarea id="info" name="info" placeholder="Enter Information about class" style="height:170px"></textarea>
+			<input type="submit" value="Add Class">
+            <button onclick="appendClass()">Add Class</button>
+            <button type="button" class="cancelbtn" onclick="document.getElementById('id47').style.display='none'" class="cancelbtn">Cancel</button>
+		</div>
+	</form>
+</div>
 
-        
+<!-- include course name number, cost price, trainer name, contact email address -->
 
         <div class="cleardiv"></div>
 
-        <div class="TrainingContainer SS">
-
-            <div class="TrainingSubTitle">
-                <h3>Strategic Studies</h3>
-            </div>
-
-            <div class="TrainingContent">
-                <div class="TrainingImg">
-                    <h3>Computer Science</h3>
-                </div>
-                
-                <div class="TrainingImg">
-                    <h3>19274</h3>
-                </div>
-
-                <div class="TrainingImg">
-                    <p>
-                        Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                        Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                    </p>
-                </div>
-
-                <div class="TrainingImg">
-                    <h3>Cost: $120</h3>
-                </div>
-
-                <div class="TrainingImg">
-                    <h3>Trainer Name</h3>
-                    <h3>Trainer.Name@Gmail.com</h3>
-                </div>
-				
-                <button type="submit" class="JoinClass" onclick="document.getElementById('id48').style.display='block'" style="width:auto;"> Join</button>
-                <input type="submit" value="Edit" class="JoinClass">
-            </div>
-
-            <div class="TrainingContent">
-                <div class="TrainingImg">
-                    <h3>Computer Science</h3>
-                </div>
-                
-                <div class="TrainingImg">
-                    <h3>19274</h3>
-                </div>
-
-                <div class="TrainingImg">
-                    <p>
-                        Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                        Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                    </p>
-                </div>
-
-                <div class="TrainingImg">
-                    <h3>Cost: $120</h3>
-                </div>
-
-                <div class="TrainingImg">
-                    <h3>Trainer Name</h3>
-                    <h3>Trainer.Name@Gmail.com</h3>
-                </div>
-
-                <input type="submit" value="Join" class="JoinClass">
-            </div>
+        <div class="TrainingSubTitle">
+            <h3>Strategic Studies</h3>
         </div>
 
-        <div class="TrainingContainer IT">
+        <table>
+            <tr>
+                <td>Strategic Studing</td>
+                <td>10672</td>
+                <td>This is a bunch of information that i have typed up to see what is going to appear on screen</td>
+                <td>$120</td>
+                <td>Bob Smith</td>
+                <td>Bob.Smith@TS.com</td>
+                <td><input type="submit" value="Join" class="JoinClass" onclick="document.getElementById('id48').style.display='block'" style="width:auto;"></td>
+                <td><input type="submit" value="Edit" class="JoinClass"></td>
+            </tr>
+            <tr>
+                <td>Course Name</td>
+                <td>Course Number</td>
+                <td>Course Info</td>
+                <td>Cost</td>
+                <td>Trainer</td>
+                <td>Trainer Email</td>
+            </tr>
+            <tr>
+                <td>Course Name</td>
+                <td>Course Number</td>
+                <td>Course Info</td>
+                <td>Cost</td>
+                <td>Trainer</td>
+                <td>Trainer Email</td>
+            </tr>
+        </table>
 
-            <div class="TrainingSubTitle">
-                <h3>IT</h3>
-            </div>
+        <div class="box">
 
-            <div class="TrainingContent">
-                <div class="TrainingImg">
-                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
-                </div>
+        </div>
+        <div class="TrainingSubTitle">
+            <h3>IT</h3>
+        </div>
+        <table>
+            <tr>
+                <td>Course Name</td>
+                <td>Course Number</td>
+                <td>Course Info</td>
+                <td>Cost</td>
+                <td>Trainer</td>
+                <td>Trainer Email</td>
+            </tr>
+        </table>
 
-                <p>
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                </p>
-                <input type="submit" value="Join" class="JoinClass">
-                <input type="submit" value="Change" class="JoinClass">
-            </div>
+        <div class="TrainingSubTitle">
+            <h3>Cyber Security</h3>
         </div>
 
-        <div class="TrainingContainer Cyber">
-
-            <div class="TrainingSubTitle">
-                <h3>Cyber Security</h3>
-            </div>
-
-            <div class="TrainingContent">
-                <div class="TrainingImg">
-                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
-                </div>
-
-                <p>
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                </p>
-                <input type="submit" value="Join" class="JoinClass">
-                <input type="submit" value="Change" class="JoinClass">
-            </div>
+        <div class="TrainingSubTitle">
+            <h3>Finance</h3>
         </div>
 
-        <div class="TrainingContainer Finance">
-
-            <div class="TrainingSubTitle">
-                <h3>Finance</h3>
-            </div>
-            <div class="TrainingContent">
-                <div class="TrainingImg">
-                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
-                </div>
-
-                <p>
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                </p>
-                <input type="submit" value="Join" class="JoinClass">
-                <input type="submit" value="Change" class="JoinClass">
-            </div>
+        <div class="TrainingSubTitle">
+            <h3>Governance</h3>
         </div>
 
-        <div class="TrainingContainer Governance">
 
-            <div class="TrainingSubTitle">
-                <h3>Governance</h3>
-            </div>
+        <!-- Database for Courses might change-->
+        <?php
+                    require 'conn.php';
+                    $count = 1;
 
-            <div class="TrainingContent">
-                <div class="TrainingImg">
-                    <img class="serviceIMG" src="picture/mountains.jpg" alt="Mountains">
-                </div>
+                    $sql = "select * from courses";
+                    $result = mysqli_query($conn, $sql) or die("Error BOOK TYPE! - " . mysqli_error($conn));
+                    echo "<table id='myTable'>";
+                    while ($row = mysqli_fetch_array($result)) {
+                        echo "<tr>";
+                        echo "<td>$row[u_courseName]</td>";
+                        echo "<td>$row[u_courseNumber]</td>";
+                        echo "<td>$row[u_courseInfo]</td>";
+                        echo "<td>$row[u_trainer]</td>";
+                        echo "<td>$row[u_trainerEmail]</td>";
+                        echo "<td><a href='#' >Join</a></td>";
+                        echo "<td><a href='#' onclick=edit($count)>edit</a></td>";
+                        echo "</tr>";
+                        $count++;
+                    }
+                    echo "</table>";
+        ?>
 
-                <p>
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                    Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.
-                </p>
-                <input type="submit" value="Join" class="JoinClass">
-                <input type="submit" value="Change" class="JoinClass">
-            </div>
-        </div>
+
+
+
+
+            
 
 
         <div id="id48" class="modal">
@@ -242,56 +191,6 @@ if (isset($_SESSION['islogin'])) {
         </div>
 
 
-        <script>
-            filterSelection("all")
-            function filterSelection(c) {
-                var x, i;
-                x = document.getElementsByClassName("TrainingContainer");
-                if (c == "all") c = "";
-                for (i = 0; i < x.length; i++) {
-                    w3RemoveClass(x[i], "show");
-                    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-                }
-            }
+    </main>
 
-            function w3AddClass(element, name) {
-                var i, arr1, arr2;
-                arr1 = element.className.split(" ");
-                arr2 = name.split(" ");
-                for (i = 0; i < arr2.length; i++) {
-                    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-                }
-            }
-
-            function w3RemoveClass(element, name) {
-                var i, arr1, arr2;
-                arr1 = element.className.split(" ");
-                arr2 = name.split(" ");
-                for (i = 0; i < arr2.length; i++) {
-                    while (arr1.indexOf(arr2[i]) > -1) {
-                        arr1.splice(arr1.indexOf(arr2[i]), 1);
-                    }
-                }
-                element.className = arr1.join(" ");
-            }
-
-
-            // Add active class to the current button (highlight it)
-            var btnContainer = document.getElementById("myBtnContainer");
-            var btns = btnContainer.getElementsByClassName("Trainingbtn");
-            for (var i = 0; i < btns.length; i++) {
-                btns[i].addEventListener("click", function () {
-                    var current = document.getElementsByClassName("active");
-                    current[0].className = current[0].className.replace(" active", "");
-                    this.className += " active";
-                });
-            }
-        </script>
-
-</main>
-
-    <!-- This is footer file -->
     <?php include("footer.php"); ?>
-
-
-
