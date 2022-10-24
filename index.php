4d526c445d9ef0723d6b4121bc4463f874d66fcc
Dylan <9025681+FluffyMan47@users.noreply.github.com>
2022-10-24 11:46:26 +1100

    Removed text after slide

--- index.php
+++ index.php
@@ -103,20 +103,8 @@ if (isset($_SESSION['islogin'])) {
         </script>
 
 
-        <!-- Text after Slide show-->
-
-
-        <h2>Welcome to Spider Web Design</h2>
-        <p>We are here to make sure your design needs are met. No fluff and fancy promises, we just deliver. Our aim is to be fast, efficient and produce the best quality products for your business.</p>
-        <p>Our passionate team work endlessly to help your business harness the power of the web. From social media to WordPress training we ensure you are given the skills to continue long after we leave the picture. We are never too busy for a chat and we are happy to provide advice and recommendations.</p>
-        <p>At Spider Web Design we bring together our experience, energy and new age designs to provide our customers with creative and innovative solutions. Our highly skilled design professionals are committed to helping your business achieve its goals. We have a number of custom design solutions including website design, business branding and promotion, including logo and banner designs as well as social media marketing.</p>
-        <p>We always go above and beyond our customer's expectations because we treat your business as our own.</p>
-        <p>We are here to make sure your design needs are met. No fluff and fancy promises, we just deliver. Our aim is to be fast, efficient and produce the best quality products for your business.</p>
-        <p>Our passionate team work endlessly to help your business harness the power of the web. From social media to WordPress training we ensure you are given the skills to continue long after we leave the picture. We are never too busy for a chat and we are happy to provide advice and recommendations.</p>
-        <p>At Spider Web Design we bring together our experience, energy and new age designs to provide our customers with creative and innovative solutions. Our highly skilled design professionals are committed to helping your business achieve its goals. We have a number of custom design solutions including website design, business branding and promotion, including logo and banner designs as well as social media marketing.</p>
-        <p>We always go above and beyond our customer's expectations because we treat your business as our own.</p>
-        <h2>Our Services</h2>
-        </div>
+        <!-- Text after Slide show might add text back-->
+    </div>
     </main>
 
     <!-- This is footer file -->
