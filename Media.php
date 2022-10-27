cf69b83cd7f636e9de277247cdf798e03983c9a3
Dylan <9025681+FluffyMan47@users.noreply.github.com>
2022-10-28 10:40:52 +1100

    Changed Media and Media.css

--- Media.php
+++ Media.php
@@ -25,69 +25,65 @@ if (isset($_SESSION['islogin'])) {
     <?php include('nav.php') ?>
 
 <main>
+
 	<div style="text-align:center">
-		<h1>Media</h1>
-	</div>
-		
-			<!-- Portfolio Gallery Grid -->
-	<div class="mediaRow">
-	  <div class="mediaColumn">
-		<div class="content">
-		  <img src="mountains.jpg" alt="Mountains" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	  <div class="mediaColumn">
-		<div class="content">
-		<img src="lights.jpg" alt="Lights" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	  <div class="mediaColumn">
-		<div class="content">
-		<img src="nature.jpg" alt="Nature" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	  <div class="mediaColumn">
-		<div class="content">
-		<img src="mountains.jpg" alt="Mountains" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	<div class="mediaColumn">
-		<div class="content">
-		  <img src="mountains.jpg" alt="Mountains" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	  <div class="mediaColumn">
-		<div class="content">
-		<img src="lights.jpg" alt="Lights" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	  <div class="mediaColumn">
-		<div class="content">
-		<img src="lights.jpg" alt="Nature" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	  <div class="mediaColumn">
-		<div class="content">
-		<img src="nature.jpg" alt="Mountains" style="width:100%">
-		  <h3>My Work</h3>
-		  <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
-		</div>
-	  </div>
-	</div>
+    <h1>Media Release</h1>
+    <p>a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words a Bunch of words </p>
+  </div>
+    <div class="cleardiv"></div>
+
+    <div class="mediaContainer">
+      <div class="mediaTitle">
+        <h3>Title</h3>
+      </div>
+      <img class="mediaIMG" src="mountains.jpg" alt="Mountains">
+      <p class="mediaInfo">
+        We are here to make sure your design needs are met. No fluff and fancy promises,>
+        Our passionate team ess WordPress training we ensure you are given the skills to continue long after we leave the picture. We are never too busy for a chat and we are happy to provide advice and recommendations.
+        At Spiderve solutions. Our highly skilled design professionals are committed to helping your business achieve its goals. We have a number of custom design solutions including website design, business branding and promotion, including logo and banner designs as well as social media marketing.
+      </p>
+      <div class="cleardiv"></div>
+    </div>
+
+    <div class="mediaContainer">
+      <div class="mediaTitle">
+        <h3>Title</h3>
+      </div>
+      <img class="mediaIMG" src="mountains.jpg" alt="Mountains">
+      <p class="mediaInfo">
+        We are here to make sure your design needs are met. No fluff and fancy promises,>
+        Our passionate team ess WordPress training we ensure you are given the skills to continue long after we leave the picture. We are never too busy for a chat and we are happy to provide advice and recommendations.
+        At Spiderve solutions. Our highly skilled design professionals are committed to helping your business achieve its goals. We have a number of custom design solutions including website design, business branding and promotion, including logo and banner designs as well as social media marketing.
+      </p>
+      <div class="cleardiv"></div>
+    </div>
+
+    <div class="mediaContainer">
+      <div class="mediaTitle">
+        <h3>Title</h3>
+      </div>
+      <img class="mediaIMG" src="mountains.jpg" alt="Mountains">
+      <p class="mediaInfo">
+        We are here to make sure your design needs are met. No fluff and fancy promises,>
+        Our passionate team ess WordPress training we ensure you are given the skills to continue long after we leave the picture. We are never too busy for a chat and we are happy to provide advice and recommendations.
+        At Spiderve solutions. Our highly skilled design professionals are committed to helping your business achieve its goals. We have a number of custom design solutions including website design, business branding and promotion, including logo and banner designs as well as social media marketing.
+      </p>
+      <div class="cleardiv"></div>
+    </div>
+
+    <div class="mediaContainer">
+      <div class="mediaTitle">
+        <h3>Title</h3>
+      </div>
+      <img class="mediaIMG" src="mountains.jpg" alt="Mountains">
+      <p class="mediaInfo">
+        We are here to make sure your design needs are met. No fluff and fancy promises,>
+        Our passionate team ess WordPress training we ensure you are given the skills to continue long after we leave the picture. We are never too busy for a chat and we are happy to provide advice and recommendations.
+        At Spiderve solutions. Our highly skilled design professionals are committed to helping your business achieve its goals. We have a number of custom design solutions including website design, business branding and promotion, including logo and banner designs as well as social media marketing.
+      </p>
+      <div class="cleardiv"></div>
+    </div>
+
 </main>
 
     <?php include("footer.php"); ?>
