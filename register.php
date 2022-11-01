f3165ee5c298a94e4b01d064ead58183ca8d0182
yjzx1214 <yjzx1214@gmail.com>
2022-11-01 14:14:27 +1100

    add sanitize to register process

--- register.php
+++ register.php
@@ -1,32 +1,25 @@
 <?php
-if(isset($_POST['SignUpGo'])){
-	
-require 'conn.php';
+if (isset($_POST['SignUpGo'])) {
 
-$username = $_POST['uname'];
-$password = $_POST['psw'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
+    include 'conn.php';
 
-$sql = "Insert Into users (u_name, u_password, u_email, u_phone, u_level) Values ('$username', '$password', '$email', '$phone', '1');";
-$result = mysqli_query($conn, $sql) or die("Error BOOK TYPE! - " . mysqli_error($conn));
-$numrows = mysqli_affected_rows($conn);
-if ($numrows == 1) {
-    // Open Session
-    session_start();
-    //save user name into session
-    $_SESSION['username'] = $username;
-    $_SESSION['userlevel'] = 1;
-    $_SESSION['islogin'] = 1;
-    setcookie('username', '', time() - 999);
-    setcookie('userlevel', time() - 999);
-    setcookie('islogin', '', time() - 999);
-    //redirect to home page
-    header('location:index.php');
-} else {
-    echo "register fail";
-}
+    $username = filter_input(INPUT_POST, 'uname', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = $_POST['psw'];
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_NUMBER_INT);
 
-
-mysqli_close($conn);
+    $sql = "INSERT Into users (u_name, u_password, u_email, u_phone, u_level) Values ('$username', '$password', '$email', '$phone', '1');";
+    if (mysqli_query($conn, $sql)) {
+        // Open Session
+        session_start();
+        //save user name into session
+        $_SESSION['username'] = $username;
+        $_SESSION['userlevel'] = 1;
+        $_SESSION['islogin'] = 1;
+        //redirect to home page
+        header('location:index.php');
+    } else {
+        echo 'Error ' . mysqli_errno($conn);
+    }
+    mysqli_close($conn);
 }
